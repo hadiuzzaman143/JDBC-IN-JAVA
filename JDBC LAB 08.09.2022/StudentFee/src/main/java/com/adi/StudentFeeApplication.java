0dc485b1634aa632fc192753c8a75efcb3b9f1e9
hadiuzzaman143 <107674015+hadiuzzaman143@users.noreply.github.com>
2022-09-09 12:13:02 +0530

    Add files via upload

---
+++ JDBC LAB 08.09.2022/StudentFee/src/main/java/com/adi/StudentFeeApplication.java
@@ -0,0 +1,13 @@
+package com.adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentFeeApplication.class, args);
+	}
+
+}
